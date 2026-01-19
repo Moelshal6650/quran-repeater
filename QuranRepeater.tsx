a45ec9690373e038b02701f4bacd5bc172abb101
Moelshal6650 <moelshal66100@gmail.com>
2026-01-19 19:37:54 +0200

    Refactor QuranRepeater to fetch ayahs dynamically

--- QuranRepeater.tsx
+++ QuranRepeater.tsx
@@ -1,40 +1,28 @@
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function QuranRepeater() {
-  const [surah, setSurah] = useState("001");
-  const [ayah, setAyah] = useState(1);
-  const [repeat, setRepeat] = useState(1);
+  const [ayahs, setAyahs] = useState<any[]>([]);
 
-  const audioRef = useRef<HTMLAudioElement | null>(null);
-  const countRef = useRef(0);
-
-  const pad = (n: number) => n.toString().padStart(3, "0");
-  const url = `https://everyayah.com/data/Yasser_Ad-Dussary_128kbps/${surah}${pad(
-    ayah
-  )}.mp3`;
-
-  const play = () => {
-    countRef.current = 0;
-    if (audioRef.current) {
-      audioRef.current.src = url;
-      audioRef.current.play();
-    }
-  };
-
-  const onEnd = () => {
-    countRef.current++;
-    if (countRef.current < repeat) {
-      audioRef.current?.play();
-    }
-  };
+  useEffect(() => {
+    fetch("https://api.alquran.cloud/v1/surah/3/ar.alafasy")
+      .then(res => res.json())
+      .then(data => setAyahs(data.data.ayahs));
+  }, []);
 
   return (
-    <div style={{ textAlign: "center", marginTop: 40 }}>
-      <h1>كَرِّر</h1>
+    <div style={{ padding: 20 }}>
+      <h2>سورة آل عمران</h2>
 
-      <input
-        placeholder="السورة (001)"
-        value={surah}
+      {ayahs.map(a => (
+        <div key={a.number}>
+          <p>{a.text}</p>
+          <audio controls src={a.audio}></audio>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}        value={surah}
         onChange={(e) => setSurah(e.target.value)}
       />
       <br /><br />
